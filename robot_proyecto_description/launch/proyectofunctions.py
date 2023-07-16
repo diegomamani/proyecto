0ebfbdcb1f6fcc3a5c5a71db90aa3eeaf05ab501
diegomamani <diego.mamani@utec.edu.pe>
2023-07-15 20:16:21 -0500

    Add files via upload
    commit final

--- robot_proyecto_description/launch/proyectofunctions.py
+++ robot_proyecto_description/launch/proyectofunctions.py
@@ -1,8 +1,30 @@
 import numpy as np
 from copy import copy
+import rbdl
 
 pi = np.pi
 
+class Robot(object):
+    def __init__(self, q0, dq0, ndof, dt):
+        self.q = q0    # numpy array (ndof x 1)
+        self.dq = dq0  # numpy array (ndof x 1)
+        self.M = np.zeros([ndof, ndof])
+        self.b = np.zeros(ndof)
+        self.dt = dt
+        self.robot = rbdl.loadModel('../urdf/robotx.urdf')
+
+    def send_command(self, tau):
+        rbdl.CompositeRigidBodyAlgorithm(self.robot, self.q, self.M)
+        rbdl.NonlinearEffects(self.robot, self.q, self.dq, self.b)
+        ddq = np.linalg.inv(self.M).dot(tau-self.b)
+        self.q = self.q + self.dt*self.dq
+        self.dq = self.dq + self.dt*ddq
+
+    def read_joint_positions(self):
+        return self.q
+
+    def read_joint_velocities(self):
+        return self.dq
 
 def dh(d, theta, a, alpha):
     """
